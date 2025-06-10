92f1029cff89eb9c2f40d4740664441b7f4934ac
Rishabh Kumar Dhirhe <kdrishabhkd@gmail.com>
2025-06-10 23:56:37 +0530

    Update Logistic_Regression_Model.py

--- Logistic_Regression_Model.py
+++ Logistic_Regression_Model.py
@@ -2,3 +2,21 @@ import numpy as np
 
 class Logistic_Regression:
     
+    # declaring learning rate and number of iterations(hyperparameters)
+    def __init__(self, learning_rate, no_of_iterations):
+        self.learning_rate = learning_rate
+        self.no_of_iterations = no_of_iterations
+
+
+    # fit function to train the model to some dataset
+    def fit(self, X, Y):
+        self.m, self.n = X.shape    #[m=rows, n=cols]
+
+        self.w = np.zeros(self.n)   # for the total number of features creating empty weights
+        self.b = 0                  # bias - initially 0
+        self.X = X                  # initiating features
+        self.Y = Y                  # intiating target
+
+        # implementing gradient descent for optimization of solution
+        for i in range(self.no_of_iterations):
+            self.update_weights()
