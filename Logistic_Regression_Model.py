51410a606156f808c2536ac3ffd2f83a0d72db29
Rishabh Kumar Dhirhe <kdrishabhkd@gmail.com>
2025-06-10 23:57:00 +0530

    Update Logistic_Regression_Model.py

--- Logistic_Regression_Model.py
+++ Logistic_Regression_Model.py
@@ -20,3 +20,15 @@ class Logistic_Regression:
         # implementing gradient descent for optimization of solution
         for i in range(self.no_of_iterations):
             self.update_weights()
+    
+    def update_weights(self):
+        # y_cap formula(sigmoid formula)
+        Y_hat = 1 / (1 + np.exp(-(self.X.dot(self.w) + self.b)))  #z = wx+b
+
+        # Now build derivatives
+        dw = (1/self.m)*np.dot(self.X.T, (Y_hat - self.Y))
+        db = (1/self.m)*np.sum(Y_hat - self.Y)
+
+        # updating weights and bias using gradient descent
+        self.w = self.w - self.learning_rate * dw
+        self.b = self.b - self.learning_rate * db
